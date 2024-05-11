b0d7461f787697368202225d48aa8f5737cbfb20
Mankelito <mahexd1@gmail.com>
2024-05-11 00:18:42 -0400

    avance

--- gametrainer/lib/main.dart
+++ gametrainer/lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'menu.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,25 +18,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'GameTrainer'),
     );
   }
 }
@@ -55,71 +45,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _goToMainMenu()
+  {
+    Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const MainMenu()),
+    );
   }
+
+  String userIcon ="assets/images/default_user.svg";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          child:Column(
+            children: [
+              const Padding(padding: EdgeInsets.all(50)),
+              SvgPicture.asset(
+                userIcon,
+                height: 150,
+                width: 150,
+              ),
+              const Padding(padding: EdgeInsets.only(top: 50)),
+              SizedBox(
+                child:Column(
+                  children: [
+                    const TextField(
+                      decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.only(top: 50)),
+                    const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.all(20)),
+                    ElevatedButton(
+                      onPressed: _goToMainMenu, 
+                      child: const Text(
+                        'Iniciar Sesion'
+                      )
+                    ),
+                  ]
+                )
+              ),
+            ],
+          ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ gametrainer/lib/menu.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gametrainer/main.dart';
+import 'game_details.dart';
+import 'profile.dart';
+import 'routine.dart';
+import 'user.dart';
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomeMenu(title: 'GameTrainer'),
+    );
+  }
+}
+
+class MyHomeMenu extends StatefulWidget {
+  
+  const MyHomeMenu({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomeMenu> createState() => _MyHomeMenu();
+}
+
+class _MyHomeMenu extends State<MyHomeMenu> {
+  
+  String trophyIcon ="assets/images/trophy-icon.svg";
+  void _goToGameDetails()
+  {
+    Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const GameDetails()),
+    );
+  }
+
+  void _goToProfile()
+  {
+    Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const Profile()),
+    );
+  }
+  void _goToRoutine()
+  {
+    Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const GameDetails()),
+    );
+  }
+
+  void _goToUser()
+  {
+    Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const Profile()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Container(
+          padding: const EdgeInsets.all(50),
+          child: Column(
+            children: <Widget> [
+              SvgPicture.asset(
+                trophyIcon,
+                height: 150,
+                width: 150,),
+              const Padding(
+                padding: EdgeInsets.only(top: 50)
+              ),
+              ElevatedButton(
+                onPressed: _goToGameDetails, 
+                child: const Text(
+                  'Detalles del juego'
+                )
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              ElevatedButton(
+                onPressed: _goToProfile, 
+                child: const Text(
+                  'Perfil de Usuario'
+                )
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              ElevatedButton(
+                onPressed: _goToRoutine, 
+                child: const Text(
+                  'Detalle de rutina'
+                )
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              ElevatedButton(
+                onPressed: _goToUser, 
+                child: const Text(
+                  'Usuario'
+                )
+              ),
+              const Padding(padding: EdgeInsets.all(10)),
+              ElevatedButton(
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyApp()),
+                ), 
+                child: const Text(
+                  'Cerrar sesion'
+                )
+              ),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
